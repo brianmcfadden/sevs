47a0d79ac0687fe1cfed9a4d1409c0ea794e7fcd
Brian McFadden <brianmmcfadden@gmail.com>
2019-07-29 17:12:19 -0400

    Allow for multiple symptoms

--- app/controllers/sevs_controller.rb
+++ app/controllers/sevs_controller.rb
@@ -1,57 +1,84 @@
 class SevsController < ApplicationController
   def index
-    @side_effects = SideEffect.all
-    @drugs = Drug.all
-    @symptoms = Symptom.all
   end
 
   def create
     @title = "Report"
+    produce_report = false
     respond_to do |format|
       Rails.logger.level = 0
 
-      @symptom = Symptom.find(params[:symptom][:id])
+      Rails.logger.info params[:symptomList].pretty_inspect
 
-      # for each drug, go through side effects for that drug
-      # and match the symptom - add to side effects list
-      # and pass that on to the view
-      @drugs = []
-      @sideEffects = []
+      @symptoms = []
+      @prescription = []
+      @drugsWithSymptoms = []
+      # @sideEffects = []
       @commonSideEffects = []
       @drugList = params[:drugList]
-      if @drugList != nil
-        @drugList.each do |drugId|
-          @drug = Drug.find(drugId)
-          @drugs.push(@drug)
-          @sideEffectsList = SideEffect.where(drug_id: drugId)
-          @sideEffectsList.each do |se|
-            #
-            @cse = @commonSideEffects.find { |cse| cse[:side].symptom_id == se.symptom_id }
-            if @cse != nil
-              @cse[:count] += 1
-            else
-              @commonSideEffects.push( { :side  => se, :count => 1} )
-            end
-            #
-            if se.symptom_id == @symptom.id
-              @sideEffects.push(se)
-            end # if symptom_id
-          end # each SideEffect
-        end # each drugList
-        # top ten-ish common side effects
-        @commonSideEffects = @commonSideEffects.sort_by { |cse| -cse[:count] }
-        @commonSideEffects.keep_if { |c| c[:count] > 1 }
-        if @commonSideEffects.length >= 10  then
-          t = @commonSideEffects[10][:count]
-          @commonSideEffects.keep_if { |c| c[:count] >= t }
+      if @drugList == nil then format.all  { redirect_to("/404.html") } end
+
+      @symptomList = params[:symptomList]
+      if @symptomList == nil then format.all  { redirect_to("/404.html") } end
+
+      #
+      # build the prescription list for the view
+      #
+      @drugList.each do |drugId|
+        @prescription.push(Drug.find(drugId))
+      end
+
+      #
+      # build the symptom list for the view
+      #
+      @symptomList.each do |symptom_id|
+        @symptoms.push(Symptom.find(symptom_id))
+      end
+
+      #
+      # build list of common side effects by going through
+      # each listed drug and counting all the symptoms
+      #
+      @prescription.each do |drug|
+        SideEffect.where(drug_id: drug.id).each do |se|
+          cse=@commonSideEffects.find { |cse| cse[:side].symptom_id==se.symptom_id }
+          if cse # != nil
+            cse[:count] += 1
+          else
+            @commonSideEffects.push( { :side  => se, :count => 1} )
+          end
         end
-        format.html {
-          render "generate_report"
-        }
-      else # if @drugList == nil
-        format.html { redirect_to("/404.html") }
-      end # if @drugList
-      format.all  { redirect_to("/404.html") }
+      end
+      #
+      # top ten-ish common side effects
+      #
+      @commonSideEffects = @commonSideEffects.sort_by { |cse| -cse[:count] }
+      @commonSideEffects.keep_if { |c| c[:count] > 1 }
+      if @commonSideEffects.length >= 10  then
+        t = @commonSideEffects[10][:count]
+        @commonSideEffects.keep_if { |c| c[:count] >= t }
+      end
+
+      #
+      # Build list of drugs with matching symptoms
+      # check each symptom across all listed drugs
+      #
+      @symptoms.each do |symptom|
+        @prescription.each do |drug|
+          SideEffect.where(drug_id: drug.id).each do |se|
+            if se.symptom_id == symptom.id
+              dws = @drugsWithSymptoms.find { |dws| dws[:drug].id == se.drug_id }
+              if dws
+                dws[:symptoms].push(symptom)
+              else
+                @drugsWithSymptoms.push({ :drug => drug,
+                                          :symptoms => [ symptom ] } )
+              end
+            end
+          end
+        end
+      end
+      format.html { render "generate_report" }
     end # respond_to
   end # def create
 end
