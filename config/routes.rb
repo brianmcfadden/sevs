16add0f409f35b195499b41c1449069ded44fcc5
Brian McFadden <brianmmcfadden@gmail.com>
2019-08-06 02:06:16 -0400

    minimize production routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,19 @@
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
-  resources :manufacturers
-  resources :classifications
-  root 'sevs#index'
-  resources :sevs
-  resources :side_effects
-  resources :symptoms
-  resources :drugs
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  if Rails.env.production? 
+  then
+    root 'sevs#index'
+    resources :sevs, only: [:index]
+    resources :side_effects, only: [:index, :show]
+    resources :symptoms, only: [:index, :show]
+    resources :drugs, only: [:index, :show]
+  else
+    resources :manufacturers
+    resources :classifications
+    root 'sevs#index'
+    resources :sevs, only: [:index]
+    resources :side_effects
+    resources :symptoms
+    resources :drugs
+  end
 end
